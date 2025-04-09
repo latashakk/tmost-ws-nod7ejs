82bf5e0742f0bf01abaf3296a6e6dfe19c4f6ae2
latashakk <latashakk@proton.me>
2025-04-09 11:03:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 const UUID = process.env.UUID || 'dd905569-f180-4bc2-a105-4dc04353d887';
 const uuid = UUID.replace(/-/g, "");
-const NEZHA_SERVER = process.env.NEZHA_SERVER || 'vuppo.singck.dpdns.org';
+const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.gvkoyeb.eu.org';
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';        // 端口为443时自动开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             // 哪吒三个变量不全不运行
-const DOMAIN = process.env.DOMAIN || '';  //项目域名或已反代的域名，不带前缀，建议填已反代的域名
+const DOMAIN = process.env.DOMAIN || 'vuppo.singck.dpdns.org';  //项目域名或已反代的域名，不带前缀，建议填已反代的域名
 const NAME = process.env.NAME || 'JP-webhostmost-GCP';
 const port = process.env.PORT || 3000;
 
