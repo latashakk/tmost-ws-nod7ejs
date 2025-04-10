e915ffbcccfd4d2a662147783f7d4f4607a57150
latashakk <latashakk@proton.me>
2025-04-10 10:43:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const { exec, execSync } = require('child_process');
 const { WebSocket, createWebSocketStream } = require('ws');
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
-const UUID = process.env.UUID || '03c90dcf-2bcc-4c4b-9fb8-057a6a54d99f';
+const UUID = process.env.UUID || '44acb9df-4417-416a-bcbe-1b5b4e5e11e4';
 const uuid = UUID.replace(/-/g, "");
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.gvkoyeb.eu.org';
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';        // 端口为443时自动开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             // 哪吒三个变量不全不运行
-const DOMAIN = process.env.DOMAIN || 'skyuup.aiseek.dpdns.org';  //项目域名或已反代的域名，不带前缀，建议填已反代的域名
+const DOMAIN = process.env.DOMAIN || 'bqq.maskyy.ddns-ip.net';  //项目域名或已反代的域名，不带前缀，建议填已反代的域名
 const NAME = process.env.NAME || 'JP-webhostmost-GCP';
 const port = process.env.PORT || 3000;
 
